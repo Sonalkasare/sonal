9020df9f4529cfe3697dacdfd1dbe7e2690e8e29
ksons <kasaresonal@gmail.com>
2024-02-01 11:37:14 +0530

    added message in multy.java

---
+++ multy.java
@@ -0,0 +1,10 @@
+public class multy {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 2;
+
+        System.out.println(" The multy is " + (a*b*c));
+    }
+}
